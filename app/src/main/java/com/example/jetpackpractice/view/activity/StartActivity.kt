4456ee759c46990ec601dcc015aaeacafa6d21a2
Satoshi_Masuko <>
2020-11-06 00:35:04 +0900

    HomeFragmentのtoolbarに戻るボタンが配置されている問題を解決

--- app/src/main/java/com/example/jetpackpractice/view/activity/StartActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/view/activity/StartActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpackpractice.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,15 +11,19 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.jetpackpractice.R
+import com.example.jetpackpractice.databinding.ActivityStartBinding
 import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityStartBinding>(this,R.layout.activity_start)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setSupportActionBar(binding.toolbar)
 
         TutorialActivity.showIfNeeded(this,savedInstanceState)
 
@@ -26,8 +31,8 @@ class StartActivity : AppCompatActivity() {
         navController = findNavController(R.id.nav_host_fragment)
         setupWithNavController(bottom_navigation,navController)
 
-        setSupportActionBar(toolbar)
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.scheduleFragment,R.id.simulateFragment,R.id.calculateFragment))
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
