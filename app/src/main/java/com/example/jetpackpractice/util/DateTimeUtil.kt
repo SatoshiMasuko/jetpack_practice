8aef5c3d212391dfa026458c22232e3253c6e1e4
Satoshi_Masuko <>
2020-11-11 13:51:08 +0900

    calculateFragmentで、季節毎に違う背景を出す処理を実装

---
+++ app/src/main/java/com/example/jetpackpractice/util/DateTimeUtil.kt
@@ -0,0 +1,30 @@
+package com.example.jetpackpractice.util
+
+import android.content.Context
+import android.media.Image
+import android.view.View
+import android.widget.ImageView
+import com.example.jetpackpractice.R
+import kotlinx.android.synthetic.main.fragment_calculate.*
+
+import java.text.SimpleDateFormat
+import java.util.*
+class DateTimeUtil {
+
+    fun backGroundSelect(imageView: ImageView){
+
+        val dateObj = Date()
+        val format = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        val display = format.format(dateObj)
+
+        when(display.substring(5, 7)){
+            "12","01","02" -> imageView.setImageResource(R.drawable.winter_image)
+            "03","04","05" -> imageView.setImageResource(R.drawable.spring_image)
+            "06","07","08" -> imageView.setImageResource(R.drawable.summer_image)
+            "09","10","11" -> imageView.setImageResource(R.drawable.autumn_image)
+        }
+    }
+}
+
+
+
--- app/src/main/java/com/example/jetpackpractice/view/fragment/CalculateFragment.kt
+++ app/src/main/java/com/example/jetpackpractice/view/fragment/CalculateFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.jetpackpractice.R
+import com.example.jetpackpractice.util.DateTimeUtil
+import kotlinx.android.synthetic.main.fragment_calculate.*
+import okhttp3.internal.Util
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class CalculateFragment : Fragment() {
@@ -17,6 +23,31 @@ class CalculateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_calculate, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        DateTimeUtil().backGroundSelect(back_ground)
+    }
+
+//    private fun backGroundSelect(){
+//
+////        val image = back_ground
+//
+//        val dateObj = Date()
+//        val format = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+//        val display = format.format(dateObj)
+//
+//        when(display.substring(5, 7)){
+//            "12","01","02" -> back_ground?.setImageResource(R.drawable.winter_image)
+//            "03","04","05" -> back_ground?.setImageResource(R.drawable.spring_image)
+//            "06","07","08" -> back_ground?.setImageResource(R.drawable.summer_image)
+//            "09","10","11" -> back_ground?.setImageResource(R.drawable.winter_image)
+//        }
+//
+//
+//
+//    }
 }
